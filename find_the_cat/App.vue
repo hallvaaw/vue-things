4ed8adc90f5e9cc26a68613a5530c5d5414d93a8
hallvaaw <hallvard_wahler@hotmail.com>
2022-11-28 23:51:03 +0100

    added next button and styling

--- find_the_cat/App.vue
+++ find_the_cat/App.vue
@@ -7,22 +7,33 @@
             <div class="relative w-[500px]">
                 <img src="./assets/display.jpeg" alt="">
                 <div v-for="picture in pictures" class="absolute inset-0 grid grid-cols-12">
-                    <div v-for="tile in picture.tiles" @click="onClick(tile)" class="bg-blue-200 border-blue-600 p-1 opacity-10 hover:bg-blue-900 cursor-default">&nbsp;</div>
+                    <div v-for="tile in picture.tiles" @click="tileClick(tile)" class="bg-blue-200 border-blue-600 p-1 opacity-10 hover:bg-blue-900 cursor-default" :class="win == true ? 'bg-gray-900 opacity-60': ''">&nbsp;</div>
                 </div>
             </div>
-            <div></div>
+            <div>
+            </div>
+                <div>
+                    <button @click="nextClick()" class="rounded text-2xl text-white font-bold py-2 px-4" :class="win == true ? 'bg-green-600': 'text-gray-400 bg-blue-600'">Next</button>
+                </div>
         </section>
+                <stopwatch :running="running" :resetWhenStart="true" />
     </main>
 </template>
 
 <script>
+import stopwatch from "./components/StopWatch.vue"
 export default {
     name: 'App',
+    components: {
+        stopwatch
+    },
     data() {
         return {
+            running: true,
             tilesArray: [],
             pictures: [],
-            correctTile: false
+            correctTile: false,
+            win: false
         }
     },
     mounted() {
@@ -38,12 +49,16 @@ export default {
         addPictures() {
             this.pictures.push({id: 1, img: "./assets/display.jpeg", tiles: this.tilesArray, correctTiles: 113})
         },
-        onClick(e) {
-            console.log(e.tileId)
-            console.log()
+        tileClick(e) {
             if (e.tileId == this.pictures[0].correctTiles) {
-                console.log("You win!")
+                this.running = false
+                this.win = true
             }
+        },
+        nextClick() {
+            this.win = false
+            this.time = 0
+            this.running = true
         }
     }
 }
