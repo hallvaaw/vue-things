f8c17c8222f1b3e2f2d34d1da6c6b8dcd3b9ce14
hallvaaw <hallvard_wahler@hotmail.com>
2022-12-02 16:25:18 +0100

    added stopwatch

---
+++ find_the_cat/components/StopWatch.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+    <p>{{ minutes }}</p>
+    <p>{{ seconds }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+    name: "stopwatch",
+    props: {
+        running: {
+            type: Boolean,
+            default: false
+        }
+    },
+    mounted() {
+        this.timeDisplay()
+    },
+    data() {
+        return { 
+            seconds: 0,
+            minutes: 0
+        }
+    },
+    methods: {
+        startTime() {
+            this.seconds++
+            if (this.seconds == 60) {
+                this.minutes++
+                this.seconds = 0
+            }
+        },
+        timeDisplay() {
+            setInterval(() => {
+                this.seconds++
+                if (this.seconds == 60) {
+                    this.minutes++
+                    this.seconds = 0
+                }
+            }, 1000)
+        }
+    }
+}
+</script>
